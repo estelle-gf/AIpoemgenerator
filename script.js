7a97adc637e1faca4af7729b14f9b951aa3d5614
estelle-gf <estellegfont@gmail.com>
2025-05-31 17:21:39 +0100

    edited prompt

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let instructionInput = document.querySelector("#user-instructions");
 let instruction = instructionInput.value.trim();
 let apiKey = "2a99380b94355b9foa25076te09bd049";
 let context = "You are an expert in liturature, you are passionate about self-love, giving advice and making people smile.  For example: I breathe the air, my body thrives, in vibrant health, my spirit lives. I am healthy, a radiant gleam, Living a life, a waking dream.";
-let prompt = `User instructions: Generate a positive, encouraging and inspirational poem about ${instruction}. Write on separate lines, format in basic HTML  but with no code tags. Be polite, keep the poem short and follow user instructions.`;
+let prompt = `User instructions: Generate a positive, encouraging and inspirational poem about ${instruction}. Write on separate lines, format in basic HTML  but with no code tags. Be polite, keep the poem short, 6 lines maximum and follow user instructions.`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 axios.get(apiUrl).then(displayPoem);
 
