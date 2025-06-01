5f813de71388edc74679aff5e14efe67cf8722ba
estelle-gf <estellegfont@gmail.com>
2025-06-01 15:14:06 +0100

    removed console.logs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function displayPoem(response) {  console.log("poem generated"); 
+function displayPoem(response) {   
  
 new Typewriter("#poemOutput", {
   strings: response.data.answer,
@@ -21,14 +21,11 @@ let instructionValue = instructionInput.value.trim();
    poemElement.classList.remove("hidden");
  poemElement.innerHTML = `<div class="generating"> ⏳Loading your ${instructionValue} poem. . .</div>`; 
 let apiKey = "2a99380b94355b9foa25076te09bd049";
-let context = "You are an expert in literature, you are passionate about self-love, giving advice and making people smile. Sign off all poems with -Shecodes AI. 6 lines maximum and follow user instructions.For example: I breathe the air, my body thrives, in vibrant health, my spirit lives. I am healthy, a radiant gleam, Living a life, a waking dream.";
+let context = "You are an expert in literature, you are passionate about self-love, giving advice and making people smile. Sign off all poems with -Shecodes AI in the same font size as the poem. 6 lines maximum and follow user instructions.For example: I breathe the air, my body thrives, in vibrant health, my spirit lives. I am healthy, a radiant gleam, Living a life, a waking dream.";
 let prompt = `User instructions: Generate a positive, encouraging and inspirational poem including the word ${instructionValue} in the response. Write on separate lines, format in basic HTML  but with no code tags. Be polite, keep the poem short.`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
-console.log("Generating poem");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`);
 axios.get(apiUrl).then(displayPoem);
 }
 
