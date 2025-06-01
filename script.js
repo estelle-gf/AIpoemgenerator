5692189c52ccea92baa430b2e355ba85a714da43
estelle-gf <estellegfont@gmail.com>
2025-06-01 15:05:36 +0100

    added blink animation for a better user interface

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
 
 function displayPoem(response) {  console.log("poem generated"); 
-  
- let poemElement = document.querySelector("#poem");
- poemElement.classList.remove("hidden");
+ 
 new Typewriter("#poemOutput", {
   strings: response.data.answer,
   autoStart: true,
@@ -13,10 +11,18 @@ new Typewriter("#poemOutput", {
 }
 function generatePoem(event) {
     event.preventDefault(); 
+
 let instructionInput = document.querySelector("#user-instructions");
+let instructionValue = instructionInput.value.trim();
+  let poemContainer = document.querySelector("#poem");
+  poemContainer.classList.remove("hidden");
+
+ let poemElement = document.querySelector("#poemOutput");
+   poemElement.classList.remove("hidden");
+ poemElement.innerHTML = `<div class="generating"> ⏳Loading your ${instructionValue} poem. . .</div>`; 
 let apiKey = "2a99380b94355b9foa25076te09bd049";
 let context = "You are an expert in literature, you are passionate about self-love, giving advice and making people smile. Sign off all poems with -Shecodes AI. 6 lines maximum and follow user instructions.For example: I breathe the air, my body thrives, in vibrant health, my spirit lives. I am healthy, a radiant gleam, Living a life, a waking dream.";
-let prompt = `User instructions: Generate a positive, encouraging and inspirational poem including the word ${instructionInput.value} in the response. Write on separate lines, format in basic HTML  but with no code tags. Be polite, keep the poem short.`;
+let prompt = `User instructions: Generate a positive, encouraging and inspirational poem including the word ${instructionValue} in the response. Write on separate lines, format in basic HTML  but with no code tags. Be polite, keep the poem short.`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
